5858521e4e2f46753cbbf9fbde17f3223c167508
khaing211 <khaing211@gmail.com>
2016-08-03 19:34:00 -0700

    Add skeleton visit to type declaration.

--- java-language/src/main/java/com/kn/language/parser/JavaEntitySpliterator.java
+++ java-language/src/main/java/com/kn/language/parser/JavaEntitySpliterator.java
@@ -13,8 +13,12 @@ import com.kn.language.core.AntlrHelper;
 import com.kn.language.core.Entity;
 import com.kn.language.core.EntitySpliterator;
 import com.kn.language.parser.antlr.java.JavaBaseVisitor;
+import com.kn.language.parser.antlr.java.JavaParser.AnnotationTypeDeclarationContext;
+import com.kn.language.parser.antlr.java.JavaParser.EnumDeclarationContext;
 import com.kn.language.parser.antlr.java.JavaParser.MarkerAnnotationContext;
 import com.kn.language.parser.antlr.java.JavaParser.NormalAnnotationContext;
+import com.kn.language.parser.antlr.java.JavaParser.NormalClassDeclarationContext;
+import com.kn.language.parser.antlr.java.JavaParser.NormalInterfaceDeclarationContext;
 import com.kn.language.parser.antlr.java.JavaParser.PackageDeclarationContext;
 import com.kn.language.parser.antlr.java.JavaParser.PackageModifierContext;
 import com.kn.language.parser.antlr.java.JavaParser.SingleElementAnnotationContext;
@@ -36,34 +40,46 @@ public class JavaEntitySpliterator extends JavaBaseVisitor<Entity>implements Ent
   @Override
   public Entity visitSingleStaticImportDeclaration(final SingleStaticImportDeclarationContext ctx) {
     // Java.g4 / singleStaticImportDeclaration
-    return AntlrHelper.fromParseRuleContext(ctx, tokenStream).type(JavaEntityType.IMPORT.name())
+    return AntlrHelper.fromParseRuleContext(ctx, tokenStream)
+        .type(JavaEntityType.IMPORT.name())
         // typeName + "." + Identifier
-        .value(AntlrHelper.getTextFromRangeOfChild(ctx, 2, 4)).property("static", true)
+        .value(AntlrHelper.getTextFromRangeOfChild(ctx, 2, 4))
+        .property("static", true)
         .property("star", false).build();
   }
 
   @Override
   public Entity visitSingleTypeImportDeclaration(final SingleTypeImportDeclarationContext ctx) {
     // Java.g4 / singleTypeImportDeclaration
-    return AntlrHelper.fromParseRuleContext(ctx, tokenStream).type(JavaEntityType.IMPORT.name())
-        .value(ctx.getChild(1).getText()).property("static", false).property("star", false).build();
+    return AntlrHelper.fromParseRuleContext(ctx, tokenStream)
+        .type(JavaEntityType.IMPORT.name())
+        .value(ctx.getChild(1).getText())
+        .property("static", false)
+        .property("star", false)
+        .build();
   }
 
   @Override
   public Entity visitTypeImportOnDemandDeclaration(final TypeImportOnDemandDeclarationContext ctx) {
     // Java.g4 / typeImportOnDemandDeclaration
-    return AntlrHelper.fromParseRuleContext(ctx, tokenStream).type(JavaEntityType.IMPORT.name())
-        .value(AntlrHelper.getTextFromRangeOfChild(ctx, 1, 3)).property("static", false)
-        .property("star", true).build();
+    return AntlrHelper.fromParseRuleContext(ctx, tokenStream)
+        .type(JavaEntityType.IMPORT.name())
+        .value(AntlrHelper.getTextFromRangeOfChild(ctx, 1, 3))
+        .property("static", false)
+        .property("star", true)
+        .build();
   }
 
   @Override
   public Entity visitStaticImportOnDemandDeclaration(
       final StaticImportOnDemandDeclarationContext ctx) {
     // Java.g4 / staticImportOnDemandDeclaration
-    return AntlrHelper.fromParseRuleContext(ctx, tokenStream).type(JavaEntityType.IMPORT.name())
-        .value(AntlrHelper.getTextFromRangeOfChild(ctx, 2, 4)).property("static", false)
-        .property("star", true).build();
+    return AntlrHelper.fromParseRuleContext(ctx, tokenStream)
+        .type(JavaEntityType.IMPORT.name())
+        .value(AntlrHelper.getTextFromRangeOfChild(ctx, 2, 4))
+        .property("static", false)
+        .property("star", true)
+        .build();
   }
 
   @Override
@@ -85,11 +101,12 @@ public class JavaEntitySpliterator extends JavaBaseVisitor<Entity>implements Ent
 
     // exclude ';' at the end
     // endIndex is inclusive so -2
-    final String value =
-        AntlrHelper.getTextFromRangeOfChild(ctx, foundIndex + 1, ctx.getChildCount() - 2);
+    final String value = AntlrHelper.getTextFromRangeOfChild(ctx, foundIndex + 1, ctx.getChildCount() - 2);
 
     final Entity entity = AntlrHelper.fromParseRuleContext(ctx, tokenStream)
-        .type(JavaEntityType.PACKAGE.name()).value(value).build();
+        .type(JavaEntityType.PACKAGE.name())
+        .value(value)
+        .build();
 
     return entity;
   }
@@ -98,23 +115,57 @@ public class JavaEntitySpliterator extends JavaBaseVisitor<Entity>implements Ent
   public Entity visitSingleElementAnnotation(final SingleElementAnnotationContext ctx) {
     // see Java.g4 / singleElementAnnotation
     // TODO: we are ignoring () bracket
-    return AntlrHelper.fromParseRuleContext(ctx, tokenStream).type(JavaEntityType.ANNOTATION.name())
-        .value(ctx.getChild(1).getText()).build();
+    return AntlrHelper.fromParseRuleContext(ctx, tokenStream)
+        .type(JavaEntityType.ANNOTATION.name())
+        .value(ctx.getChild(1).getText())
+        .build();
   }
 
   @Override
   public Entity visitMarkerAnnotation(final MarkerAnnotationContext ctx) {
     // see Java.g4 / markerAnnotation
-    return AntlrHelper.fromParseRuleContext(ctx, tokenStream).type(JavaEntityType.ANNOTATION.name())
-        .value(ctx.getChild(1).getText()).build();
+    return AntlrHelper.fromParseRuleContext(ctx, tokenStream)
+        .type(JavaEntityType.ANNOTATION.name())
+        .value(ctx.getChild(1).getText())
+        .build();
   }
 
   @Override
-  public Entity visitNormalAnnotation(NormalAnnotationContext ctx) {
+  public Entity visitNormalAnnotation(final NormalAnnotationContext ctx) {
     // see Java.g4 / normalAnnotation
     // TODO: we are ignoring () bracket
-    return AntlrHelper.fromParseRuleContext(ctx, tokenStream).type(JavaEntityType.ANNOTATION.name())
-        .value(ctx.getChild(1).getText()).build();
+    return AntlrHelper.fromParseRuleContext(ctx, tokenStream)
+        .type(JavaEntityType.ANNOTATION.name())
+        .value(ctx.getChild(1).getText())
+        .build();
+  }
+
+  @Override
+  public Entity visitNormalClassDeclaration(final NormalClassDeclarationContext ctx) {
+    // See Java.g4 / normalClassDeclaration
+    // TODO Auto-generated method stub
+    return super.visitNormalClassDeclaration(ctx);
+  }
+
+  @Override
+  public Entity visitEnumDeclaration(final EnumDeclarationContext ctx) {
+    // See Java.g4 / enumDeclaration
+    // TODO Auto-generated method stub
+    return super.visitEnumDeclaration(ctx);
+  }
+
+  @Override
+  public Entity visitNormalInterfaceDeclaration(final NormalInterfaceDeclarationContext ctx) {
+    // See Java.g4 / normalInterfaceDeclaration
+    // TODO Auto-generated method stub
+    return super.visitNormalInterfaceDeclaration(ctx);
+  }
+
+  @Override
+  public Entity visitAnnotationTypeDeclaration(final AnnotationTypeDeclarationContext ctx) {
+    // See Java.g4 / annotationTypeDeclaration
+    // TODO Auto-generated method stub
+    return super.visitAnnotationTypeDeclaration(ctx);
   }
 
   @Override
